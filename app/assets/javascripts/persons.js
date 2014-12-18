ecd90b016d2bf326c6d5e3a27a12071a78ccf7c7
seanrlarge <sean.large1@gmail.com>
2014-12-17 20:19:19 -0500

    changes to views to show user a viz of their network on their home page

---
+++ app/assets/javascripts/persons.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    //todo dynamic configuration
+    config.nodeTypes = { type : ["User","Person"]}
+    config.nodeCaption=function(n) {return n.name || n.title;};
+    config.edgeCaption={"caption":["KNOWS"]};
+    config.nodeMouseOver = function(n) {return n.id + "<br/>"+n.name || n.title;};
+
+
+    config.neo4jUrl="http://localhost:7474/db/data";
+    new Cy2Neo(config,"graph","cypher","execute", function() { return $("#neo4jUrl").val(); });
+  });
